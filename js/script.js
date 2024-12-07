ac8172cd4a8fecd3d6073aa1ab3e807b31d734ad
Yawen Luo <carmluo@umich.edu>
2024-12-07 17:22:05 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let ingredientCheckbox = document.querySelectorAll("input[type=checkbox]")
 let resetBtn = document.querySelector("#reset")
 
-
+// if ingredient is checked off, add line crossthrough and move it to the bottom of the list
 ingredientCheckbox.forEach(function(checkbox){
     checkbox.addEventListener('change', function(){
         let ingredient = checkbox.parentElement;
@@ -19,6 +19,7 @@ ingredientCheckbox.forEach(function(checkbox){
     })
 })
 
+// reset button js
 resetBtn.addEventListener("click", function(event){
     event.preventDefault();
     ingredientCheckbox.forEach(function(checkbox){
@@ -30,6 +31,7 @@ resetBtn.addEventListener("click", function(event){
     })
 })
 
+// if instruction is clicked, add cross through
 let instructions = document.querySelectorAll(".instruction")
 instructions.forEach(function(text){
     text.value = 0;
@@ -47,16 +49,25 @@ instructions.forEach(function(text){
             text.style.textDecoration = "none";
             text.style.color = "#000000"
         }
-        // if (checkbox.checked === true){
-        //     console.log(`checked: ${ingredient.innerText}`);
-        //     ingredient.style.order = 1;
-        //     ingredient.style.textDecoration = "line-through";
-        //     ingredient.style.color = "#929989";
-        // } else {
-        //     console.log(`unchecked: ${ingredient.innerText}`);
-        //     ingredient.style.order = 0;
-        //     ingredient.style.textDecoration = "none";
-        //     ingredient.style.color = "#000000";
-        // }
+    })
+})
+
+// same as instructions but detects clicking on mobile as click is exclusively mouse click
+instructions.forEach(function(text){
+    text.value = 0;
+    text.addEventListener('touchend', function(){
+        let instruction = text.innerText;
+        if (text.value == 0){
+            text.value = 1;
+            console.log(`!was clicked !value is ${text.value}`)
+            text.style.textDecoration = "line-through #F25B50 2px";
+            // text.style.textDecorationColor = "#F25B50";
+            text.style.color = "#929989";
+        } else {
+            console.log(`unclicked`)
+            text.value = 0;
+            text.style.textDecoration = "none";
+            text.style.color = "#000000"
+        }
     })
 })
